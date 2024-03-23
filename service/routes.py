67ef03dd09d38b92ebc617eecde060e0feb6c5f2
DBow899 <dpbower899@gmail.com>
2024-03-22 20:18:12 -0400

    updated ci work addressing latest error

--- service/routes.py
+++ service/routes.py
@@ -91,7 +91,7 @@ def get_accounts(account_id):
     Reads an Account
     This endpoint will read an Account based the account_id that is requested
     """
-     app.logger.info("Request to read an Account with id: %s", account_id)
+    app.logger.info("Request to read an Account with id: %s", account_id)
 
     account = Account.find(account_id)
     if not account:
